a3eae240eb27415aabaea641f7d32e956138b9dd
commit-panda <sjmanyarkiy@gmail.com>
2025-12-02 16:03:53 +0300

    passed tests

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -3,11 +3,11 @@
 import { pointsForWord } from '../utils'
 
 describe("pointsForWord", () => {
-    it("calculates the total points for a word (1 point per vowel, 2 per consonant)", () => {
-        const word = "test";
+    it("handles uppercase and lowercase input", () => {
+  const word = "tEsT";
 
-        const points = pointsForWord(word);
+  const points = pointsForWord(word);
 
-        expect(points).toBe(7);
-    })
+  expect(points).toBe(7);
+});
 })
